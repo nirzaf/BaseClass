1a4bda58b9acba8a7ed20500fa87afa3248adb88
nirzaf <mfmfazrin1986@gmail.com>
2022-02-10 16:15:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var bicycle = new Bicycle();
+    }
+}
+
+
+class Vehicle
+{
+    public int NumberOfWheels { get; set; }
+
+    public Vehicle(int numberOfWheels)
+    {
+        NumberOfWheels = numberOfWheels;
+        PrintOutDescription();
+    }
+
+
+    protected virtual void PrintOutDescription()
+    {
+        Console.WriteLine($"The vehicle has {NumberOfWheels} wheels");
+    }
+}
+
+
+class Bicycle : Vehicle
+{
+    public Bicycle() : base(2)
+    {
+    }
+
+    protected override void PrintOutDescription()
+    {
+        Console.WriteLine($"The bicycle has {NumberOfWheels} wheels");
+    }
+}
